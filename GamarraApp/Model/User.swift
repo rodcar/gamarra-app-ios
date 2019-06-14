37a31a1e1409bb0090c112cfbeca32b4beecc037
user155748 <user155748@74-80-250-230.ic2net.net>
2019-06-14 00:06:51 -0700

    Agrega el modelo User

---
+++ GamarraApp/Model/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  GamarraApp
+//
+//  Created by user155748 on 6/13/19.
+//  Copyright © 2019 Developer User. All rights reserved.
+//
+
+import Foundation
+
+class User: Codable {
+    var id: Int
+    var username: String?
+    var password: String?
+}
