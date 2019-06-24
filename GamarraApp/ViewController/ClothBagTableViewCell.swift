38a6a7f0be14de1ae18f1400491994c727889bc4
user155748 <ivanrod12@hotmail.com>
2019-06-23 22:39:54 -0700

    Agrega mostra imagen de prenda en la seccion de bolsa

---
+++ GamarraApp/ViewController/ClothBagTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClothBagTableViewCell.swift
+//  GamarraApp
+//
+//  Created by user155748 on 6/23/19.
+//  Copyright © 2019 Developer User. All rights reserved.
+//
+
+import UIKit
+
+class ClothBagTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
