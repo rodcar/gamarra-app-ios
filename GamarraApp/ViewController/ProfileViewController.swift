f960fd04a5f22e872e3aade7517e90df32b57ce9
user155748 <ivanrod12@hotmail.com>
2019-06-25 22:30:17 -0700

    Agrega menu de opciones en el perfil

--- GamarraApp/ViewController/ProfileViewController.swift
+++ GamarraApp/ViewController/ProfileViewController.swift
@@ -7,10 +7,18 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ProfileViewController: ViewController {
 
+    @IBOutlet weak var welcomeMessageLabel: UILabel!
+    
     var defaults = UserDefaults.standard
+    var options : [String] = [
+        "Negocios",
+        "Suscripcion"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,12 +27,38 @@ class ProfileViewController: ViewController {
         self.navigationItem.hidesBackButton = true
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        let userId = defaults.integer(forKey: "id")
+        let accessToken = defaults.string(forKey: "accessToken")
+        
+        let headers: HTTPHeaders = [
+            "Authorization": "Bearer \(accessToken ?? "" )"
+        ]
+        
+        AF.request("https://quiet-temple-50701.herokuapp.com/users/\(userId)", headers: headers).responseJSON {
+            response in
+            switch response.result {
+            case let.success(value):
+                let responseStatusCode = response.response?.statusCode
+                if responseStatusCode == 200 {
+                    let user = JSON(value).dictionaryValue
+                    self.welcomeMessageLabel.text = "Bienvenido \(user["name"]!.stringValue)"
+                }
+                break
+            case let .failure(error):
+                break
+            }
+        }
+    }
+    
+    
     @IBAction func logoutButtonItemTapped(_ sender: Any) {
         defaults.set(0, forKey: "id")
         defaults.set("", forKey: "accessToken")
         self.performSegueToReturnBack()
     }
 
+    
     /*
     // MARK: - Navigation
 
@@ -35,4 +69,20 @@ class ProfileViewController: ViewController {
     }
     */
 
+}
+
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "profileOptionCell", for: indexPath)
+        cell.textLabel?.text = options[indexPath.row]
+        return cell
+    }
+    
+    
 }
